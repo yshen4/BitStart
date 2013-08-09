c84e1a8704ebdd2dfbed86362a3f3d053a4931a8
Ubuntu <ubuntu@ip-172-31-35-156.us-west-2.compute.internal>
2013-08-09 05:01:57 +0000

    Edicted product desc in index.html

--- grader.js
+++ grader.js
@@ -66,6 +66,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to page.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
--- web.js
+++ web.js
@@ -1,5 +1,4 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
@@ -8,7 +7,7 @@ app.get('/', function(request, response) {
   response.send(contents);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
