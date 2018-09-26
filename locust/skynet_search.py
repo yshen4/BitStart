0e5ef142dd752fe0e5cc249c923b25319ce64561
yshen4 <shyue2010@gmail.com>
2018-09-25 22:52:40 -0700

    Update

--- locust/skynet_search.py
+++ locust/skynet_search.py
@@ -15,7 +15,12 @@ class SkynetTasks(TaskSet):
              })
 
 class SkynetUser(HttpLocust):
+    # task_set point to a TaskSet
     task_set = SkynetTasks
+
+    # replaced with --host in locust command line
     host = 'https://www.video110.cn:8030' 
+
+    # config
     min_wait = 5000
     max_wait = 15000
